6a9fae832c646a2cf208a5f17d3dbda3d5a6ba1f
Serdar Turkmen <baver_t@hotmail.com>
2018-04-15 13:51:15 +0300

    Initial application generated by JHipster-4.14.3

---
+++ src/main/java/com/experimental/firstservice/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.experimental.firstservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Firstservice.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
---
+++ src/main/java/com/experimental/firstservice/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.experimental.firstservice.security;
+
+import com.experimental.firstservice.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        return SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT);
+    }
+}
